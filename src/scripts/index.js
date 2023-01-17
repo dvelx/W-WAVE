c135b5a22f0ad1668c89a5d8c5ba19057abb2ec3
prostoG <49344670+prostoG@users.noreply.github.com>
2023-01-17 20:51:21 +0300

    swiper about-us footer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,4 +2,12 @@
 
 new Accordion('.accordion-container');
 
-console.log('ggg');
+const swiperAboutUs = new Swiper('.about-us__swiper', {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  slideClass: 'swiper-slide',
+  slidesPercolumn: 1,
+  autoplay: {
+    delay: 6000,
+  }
+})
